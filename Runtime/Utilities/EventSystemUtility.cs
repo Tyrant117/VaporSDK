9a9bc89935f28f6b677c380d12a5bb0588bcd39d
Tyrant117 <Tyrant117@users.noreply.github.com>
2024-05-31 14:05:46 -0700

    Event System Fix

--- Runtime/Utilities/EventSystemUtility.cs
+++ Runtime/Utilities/EventSystemUtility.cs
@@ -1,12 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
 
 namespace Vapor
 {
     public static class EventSystemUtility
     {
+        private static InputSystemUIInputModule s_Module;
+
         /// <summary>
         /// Checks if the pointer is over a GUI element
         /// </summary>
@@ -14,6 +15,17 @@ namespace Vapor
         public static bool IsPointerOverGUI()
         {
             return EventSystem.current && EventSystem.current.IsPointerOverGameObject();
+        }
+
+        public static bool IsPointerOverGUIAction()
+        {
+            if (!EventSystem.current)
+                return false;
+
+            if (!s_Module)
+                s_Module = (InputSystemUIInputModule)EventSystem.current.currentInputModule;
+
+            return s_Module.GetLastRaycastResult(Mouse.current.deviceId).isValid;
         }
     }
 }
